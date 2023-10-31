eaae4760a0519825da14ae378b6b03a70e1bbf81
Ansar Ismagilov <ansar89kab@bk.ru>
2023-10-31 20:16:54 +0800

    init commit

---
+++ src/main/java/com/kabgig/jenkinsTestProject1/JenkinsTestProject1Application.java
@@ -0,0 +1,13 @@
+package com.kabgig.jenkinsTestProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestProject1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/kabgig/jenkinsTestProject1/helloworld.java
@@ -0,0 +1,12 @@
+package com.kabgig.jenkinsTestProject1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class helloworld {
+    @Bean
+    public void print(){
+        System.out.println("Hello madafakas!!!");
+    }
+}
