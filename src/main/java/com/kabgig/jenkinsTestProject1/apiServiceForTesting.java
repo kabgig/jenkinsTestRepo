784c9502a82abb02c081d484341f7f3650214133
Ansar Ismagilov <ansar89kab@bk.ru>
2023-11-05 18:07:12 +0800

    changed to hello world

--- src/main/java/com/kabgig/jenkinsTestProject1/apiServiceForTesting.java
+++ src/main/java/com/kabgig/jenkinsTestProject1/apiServiceForTesting.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class apiServiceForTesting {
 
-    @GetMapping("/madafakas!")
+    @GetMapping("/helloworld")
     public String helloworld(){
-        return "Hello madafakas!!!";
+        return "Hello world!!!";
     }
 }
